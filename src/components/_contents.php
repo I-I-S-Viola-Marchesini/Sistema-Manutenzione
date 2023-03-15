480fcaf1f86cb9866a7888bb29c8be66c8e24144
alex65ferrari <41290578+PaninoCode@users.noreply.github.com>
2023-03-15 13:12:34 +0100

    modificato header

--- src/components/_contents.php
+++ src/components/_contents.php
@@ -1,3 +1,3 @@
-<div class="overflow-y-scroll">
-    <h1>Viva la manutenzione e la prevenzione</h1>
-</div>
+<script>
+    alert("No token");
+</script>
--- src/components/_header.php
+++ src/components/_header.php
@@ -1,18 +1,25 @@
-<nav class="navbar bg-primary navbar-dark shadow">
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark shadow">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Sistema Manutenzione</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" href="#offcanvas" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <?php
+      include 'components/_menu.php';
+      ?>
+    </div>
   </div>
 </nav>
 
 <div class="offcanvas bg-primary-subtle offcanvas-start" tabindex="-1" id="offcanvas" aria-labelledby="offcanvasLabel">
   <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasLabel">Offcanvas</h5>
+    <h5 class="offcanvas-title" id="offcanvasLabel">Sistema Manutenzione</h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
   </div>
   <div class="offcanvas-body">
-    Content for the offcanvas goes here. You can place just about any Bootstrap component or custom elements here.
+    <?php
+    include 'components/_menu.php';
+    ?>
   </div>
 </div>
